77da09ac6f6127a754d25d8cd56e507236620275
Bekzod0206 <davronovbekzod20@gmail.com>
2024-11-18 21:48:11 +0500

    fixed issues

--- src/components/statistics.jsx
+++ src/components/statistics.jsx
@@ -4,8 +4,8 @@ import { statistics } from '../utils/constants'
 function Statistics() {
   return (
     <div className={`${styles.flexStart} flex-row flex-wrap sm:mb-20 mb-6`}>
-      {statistics.map((stat) => (
-        <div key={stat.id} className="flex-1 flex justify-start items-center flex-row m-3">
+      {statistics.map((stat, idx) => (
+        <div key={idx} className="flex-1 flex justify-start items-center flex-row m-3">
           <h4 className="font-montserrat font-semibold xs:text-[41px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
             { stat.value }
           </h4>
